e9cdb0f4f17cd00179ea4681e2c8c641b1e55caf
larryhudson <larryhudson@hey.com>
2022-02-17 17:36:39 +1100

    Add componentBlocks option

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,18 @@
 const reactDOMServer = require("react-dom/server");
 const { DocumentRenderer } = require("@keystone-6/document-renderer");
 
-const renderDocumentToString = (document) => {
+const renderDocumentToString = ({document, componentBlocks}) => {
   return reactDOMServer.renderToString(
     DocumentRenderer({
       document,
+      componentBlocks
     })
   );
 };
 
-module.exports = function (eleventyConfig) {
+module.exports = function (eleventyConfig, {componentBlocks}) {
+//   componentBlocks should be transpiled from JSX before supplying to the plugin?
   eleventyConfig.addShortcode("keystoneDocument", function (document) {
-    return renderDocumentToString(document);
+    return renderDocumentToString({document, componentBlocks});
   });
 };
